2ffe2c9520fbf1a74177b840bb77d20390f0aa1d
AlexRamosBrazil <eng.alexramos@gmail.com>
2024-05-08 17:21:20 -0300

    commit com o codigo proposto e funcional

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log ("Hello word!")
+
+
+let vitorias = 300;
+let derrotas = 80;
+
+function calcularSaldoVitorias(vitorias, derrotas) {
+    let saldoVitorias;
+    saldoVitorias = vitorias - derrotas;
+    definirRanking(saldoVitorias)
+}
+
+function definirRanking(saldo){
+
+    let rank;
+
+    if (saldo < 10){
+        rank = "Ferro";
+    } else if (saldo < 20) {
+        rank = "Bronze";
+    } else if (saldo < 50) {
+        rank = "Prata";
+    } else if (saldo < 80) {
+        rank = "Ouro";
+    } else if (saldo < 90) {
+        rank = "Diamante";
+    } else if (saldo < 100) {
+        rank = "Lendário";
+    } else {
+        rank = "Imortal";
+    }
+
+    console.log ("O Herói tem de saldo de " + saldo + " está no nível de " + rank);
+}
+
+calcularSaldoVitorias(vitorias, derrotas);
